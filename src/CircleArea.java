2a55d4daa90bf74f63e39fab76b3325d8d6302ca
Omar Shaikh <151953159+senseofomar@users.noreply.github.com>
2024-07-07 16:30:48 +0530

    Update CircleArea.java

--- src/CircleArea.java
+++ src/CircleArea.java
@@ -1,6 +1,12 @@
 public class CircleArea {
     public static void main(String[] args) {
         // TODO: 04-07-2024 calculate Circle Area
+     Scanner scanner = new Scanner(System.in);
+     System.out.print("Enter the radius of the circle: ");
+      double radius = scanner.nextDouble();
+      double area = Math.PI * Math.pow(radius, 2);
+    System.out.println("Area of the circle with radius " + radius + " units: " + area);
+      scanner.close();
 
     }
 }
