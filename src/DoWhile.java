7d3b0ae175a831cf3844b667636a19d3bb187fe0
ifahimkhan <khan.fahim95@hotmail.com>
2024-07-08 11:31:03 +0530

    added import statement

--- src/DoWhile.java
+++ src/DoWhile.java
@@ -1,3 +1,12 @@
 public class DoWhile {
-    // TODO: 05-07-2024 print Numbers 1 to 10 using do while loop
+    public static void main(String[] args) {
+
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        }
+        while (i < 11);
+    }
 }
+    // TODO: 05-07-2024 print Numbers 1 to 10 using do while loop
--- src/Voting.java
+++ src/Voting.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Voting {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your age: ");
         int age = scanner.nextInt();
