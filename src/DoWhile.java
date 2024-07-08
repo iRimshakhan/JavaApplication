ff21059feddf663579d597f89f7b7ccd40c627c1
ifahimkhan <khan.fahim95@hotmail.com>
2024-07-08 11:58:35 +0530

    import file

--- src/DoWhile.java
+++ src/DoWhile.java
@@ -2,14 +2,23 @@ public class DoWhile {
     public static void main(String[] args) {
         // TODO: 05-07-2024 print Numbers 1 to 10 using do while loop
 
-        int i = 1;
+          int i = 1;
+          do {
+              System.out.println(i);
+              i++;
+          }
+          while (i < 11);
+
+        System.out.println("\n");
+        // TODO: 05-07-2024 print Numbers 10 to 1 using do while loop
+
+        int j = 10;
         do {
-            System.out.println(i);
-            i++;
+            System.out.println(j);
+            j--;
         }
-        while (i < 11);
+        while (j > 0);
     }
-    // TODO: 05-07-2024 print Numbers 10 to 1 using do while loop
-
-
 }
+
+
