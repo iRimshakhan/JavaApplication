ff1008f0f53824e5b512146f2198b3b55f49127c
ifahimkhan <khan.fahim95@hotmail.com>
2024-07-08 11:20:21 +0530

    question added in todo

--- src/PrintDaysUsingIfElse.java
+++ src/PrintDaysUsingIfElse.java
@@ -1,5 +1,29 @@
 public class PrintDaysUsingIfElse {
     public static void main(String[] args) {
         // TODO: 04-07-2024 Use If else logic to Print days
+        int day=5;
+        switch(day){
+            case 1 :
+                System.out.println("Monday");
+            break;
+            case 2 :
+                System.out.println("Tuesday");
+            break;
+            case 3 :
+                System.out.println("Wednesday");
+            break;
+            case 4 :
+                System.out.println("Thursday");
+            break;
+            case 5 :
+                System.out.println("Friday");
+                break;
+            case 6 :
+                System.out.println("Saturday");
+                break;
+            case 7 :
+                System.out.println("Sunday");
+                break;
+        }
     }
 }
