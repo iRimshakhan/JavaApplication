a3a8ecad377f37b82029a7fe6a7e5076e6e90c56
ifahimkhan <khan.fahim95@hotmail.com>
2024-07-08 11:40:28 +0530

    added todo task

--- src/DoWhile.java
+++ src/DoWhile.java
@@ -1,5 +1,6 @@
 public class DoWhile {
     public static void main(String[] args) {
+        // TODO: 05-07-2024 print Numbers 1 to 10 using do while loop
 
         int i = 1;
         do {
@@ -8,5 +9,7 @@ public class DoWhile {
         }
         while (i < 11);
     }
+    // TODO: 05-07-2024 print Numbers 10 to 1 using do while loop
+
+
 }
-    // TODO: 05-07-2024 print Numbers 1 to 10 using do while loop
