d298ec964b01c26960f9ad51e475b2f3f116a0f6
yasin <muhammedyasinshaikh48@gmail.com>
2024-07-11 20:40:41 +0530

    commited 1 to 100 odd and even numbers

--- src/PrintOddNumbers.java
+++ src/PrintOddNumbers.java
@@ -1,21 +1,10 @@
 public class PrintOddNumbers {
     // TODO: 08-07-2024 Task is to print ODD numbers between 1 to 100
     public static void main(String[] args) {
-        for(int num=1; num<=100; num+=1) {     //using for loop
-            if (num % 2 == 0)   // checking the no. is even or not
-                continue;  //skip that iteration and continue
-            else {
-                System.out.println(num);
-            }
-        }
+        System.out.print("odd numbers from 1 to 100: ");
 
-        System.out.println("\n\n");
-        int Num=1;
-        while(Num<=100){
-            System.out.println(Num);
-            Num += 2;  // num=num+2
+        for (int num = 1; num <= 100; num += 2){
+            System.out.print(num + " ");
         }
-
     }
-
 }
